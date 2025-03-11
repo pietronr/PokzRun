574211349cb72f2df3c8ced8b1afa0ad45f80817
Pietro Roque <pietronieroboss@gmail.com>
2025-03-11 19:59:03 -0300

    feat: Rigidbody and Colliders. Player now collides with obstacles and walls, changing their color.

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -6,10 +6,23 @@ public class Mover : MonoBehaviour
 
     void Start()
     {
+        PrintInstructions();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        MovePlayer();
+    }
+
+    private void PrintInstructions() 
+    {
+        Debug.Log("Welcome to the game");
+        Debug.Log("Move your player with WASD or arrow keys");
+        Debug.Log("Don't hit the walls!");
+    }
+
+    private void MovePlayer()
     {
         float xMoveSpeed = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float yMoveSpeed = 0.0f;
